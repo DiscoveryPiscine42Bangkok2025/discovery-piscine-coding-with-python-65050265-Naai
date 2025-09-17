d02d92144909a7788c5bd2c5c45f51fcc017704d
65050265-Naai <65050265@kmitl.ac.th>
2025-09-17 13:59:29 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age=int(input('tell your age : '))
+
+print('you are currently',age,'years old.')
+print("In 10 years, You'll be",age+10,"years old.")
+print("In 20 years, You'll be",age+20,"years old.")
+print("In 30 years, You'll be",age+30,"years old.")
