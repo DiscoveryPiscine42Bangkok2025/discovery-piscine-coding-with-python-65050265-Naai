9dd080935702bb42711ce080dc3a7cd54eebf805
65050265-Naai <65050265@kmitl.ac.th>
2025-09-17 14:34:04 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+num1=int(input('give me first number : '))
+num2=int(input('give me second number : '))
+
+print(num1,'+',num2,'=',num1+num2)
+print(num1,'-',num2,'=',num1-num2)
+print(num1,'x',num2,'=',num1*num2)
+
+if num2 != 0:
+    print(num1,'/',num2,'=',num1/num2)
+else:
+    print("Error")
