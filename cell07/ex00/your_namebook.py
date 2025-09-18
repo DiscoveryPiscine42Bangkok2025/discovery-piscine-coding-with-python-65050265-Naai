616ad3741954c1bdf86cde7e023cb5a1ab78a518
65050265-Naai <65050265@kmitl.ac.th>
2025-09-18 19:42:04 +0700

    Add files via upload

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,9 @@
+def array_of_names(dic):
+    return [f"{key.capitalize()} {value.capitalize()}" for key, value in dic.items()]
+persons = {
+ "jean": "valjean",
+ "grace": "hopper",
+ "xavier": "niel",
+ "fifi": "brindacier"
+}
+print(array_of_names(persons))
